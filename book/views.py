94dd06468db2c3ce06a3233da519829444f8a052
Abdelrahman Emad <ABDO@localhost.localdomain>
2020-10-03 15:17:14 +0200

    last

--- book/views.py
+++ book/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import Book
 
 # Create your views here.
-def book_detail(request):
-    context = {}
+
+
+def book_detail(request, id):
+    book_detail = Book.objects.get(id =id)
+    context = {'book' : book_detail}
     return render(request, 'book_detail.html', context)
